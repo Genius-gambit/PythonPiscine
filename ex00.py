74aa1acf67c4b3d1b1b9d4720bc622997df23e6a
Muazzam Akhtar <81755254+Genius-gambit@users.noreply.github.com>
2023-09-07 15:58:14 +0400

    Create ex00.py

---
+++ ex00.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Universe!")
+ft_set = {"Hello", "Galaxy!"}
+ft_dict = {"Hello" : "Evaluator"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
