ea654b0379b5c6448707d10ae4bc487aa28928e1
Muazzam Akhtar <81755254+Genius-gambit@users.noreply.github.com>
2023-11-01 16:24:52 +0400

    Add files via upload

--- ex05/building.py
+++ ex05/building.py
@@ -1,28 +1,29 @@
 import argparse
 import sys
 
-def	calcString(str):
+def	calcString(_str):
 	upperCaseLetters = 0
 	lowerCaseLetters = 0
 	digits = 0
 	spaces = 0
 	punctuations = 0
-	for i in str:
+	for i in _str:
 		if i.isupper():
 			upperCaseLetters += 1
 		elif i.islower():
 			lowerCaseLetters += 1
 		elif i.isdigit():
 			digits += 1
-		elif i.isspace():
+		elif i.isspace() or i == '\r' or i == '\n':
 			spaces += 1
 		else:
 			punctuations += 1
-	print(upperCaseLetters)
-	print(lowerCaseLetters)
-	print(digits)
-	print(spaces)
-	print(punctuations)
+	print("The text contain " + str(len(_str)) + " letters")
+	print(str(upperCaseLetters) + " upper letters")
+	print(str(lowerCaseLetters) + " lower letters")
+	print(str(punctuations) + " punctuation marks")
+	print(str(spaces) + " spaces")
+	print(str(digits) + " digits")
 
 def main():
 	parser = argparse.ArgumentParser(prog='ProgramName')
@@ -30,7 +31,7 @@ def main():
 
 	_list = sys.argv
 	if len(_list) == 1:
-		user_input = input("")
+		user_input = input("What is the text to count?\n")
 		__name__ = '__calcString__'
 		if __name__ == '__calcString__':
 			calcString(user_input)
