db02ad1411ef2595676c0ba0dc9b3e5865faffe7
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-22 09:28:08 +0400

    Add files via upload

---
+++ p01/ex01/array2D.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def	slice_me(family: list, start: int, end: int) -> list:
+	try:
+		if not isinstance(family, list) \
+				or not isinstance(start, int) or not isinstance(end, int):
+			raise AssertionError("Input must be a list and 2 integers.")
+		if not all(len(item) == len(family[0]) for item in family):
+			raise AssertionError("Input list with different sizes.")
+		print(f"My shape is : {np.array(family).shape}")
+		print(f"My new shape is : {np.array(family)[start:end].shape}")
+		return np.array(family)[start:end].tolist()
+	except AssertionError as error:
+		print("\033[31m", AssertionError.__name__ + ":", error, "\033[0m")
+		return ""
+	
+if	__name__ == '__main__':
+	family = [[1.80, 78.4],
+			[2.15, 102.7],
+			[2.10, 98.5],
+			[1.88, 75.2]]
+	print("\n      TEST 1\n")
+	print(slice_me(family, 0, 2))
+	print("\n      TEST 2\n")
+	print(slice_me(family, 1, -2))
+	print("\n      TEST 3\n")
+	family = 10
+	print(slice_me(family, 0, 2))
+	print("\n      TEST 4\n")
+	family = [[1.80, 78.4],
+			[2.15, 102.7],
+			[2.10, 98.5, 2.15],
+			[1.88, 75.2]]
+	print(slice_me(family, 0, 2))
+	print("\n      TEST 5\n")
+	family = [[1.80, 78.4],
+			[2.15, 102.7],
+			[2.10, 98.5],
+			[1.88, 75.2]]
+	print(slice_me(family, "a", 2))
